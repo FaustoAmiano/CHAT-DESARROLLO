2f61ba747d36fcf1e8daba29094b692730178842
Victoria Stagno <104151788+FaustoAmiano@users.noreply.github.com>
2023-09-25 10:15:54 -0300

    updste

--- public/js/Web_Socket.js
+++ public/js/Web_Socket.js
@@ -8,17 +8,22 @@ socket.on("connect", () => {
 
 function funcionPrueba() {
     textoEnviado= document.getElementById("msj").value
-    data={
-        mensaje: textoEnviado
+    if (textoEnviado==""){
+      console.log("No escribio nada")
+    }else{
+      data={
+          mensaje: textoEnviado
+      }
+      socket.emit("incoming-message", { mensaje: textoEnviado });
+      document.getElementById("msj").value="";
     }
-    socket.emit("incoming-message", { mensaje: textoEnviado });
 }
 socket.on("server-message", data => {
     console.log("Me llego del servidor", data);
     document.getElementById("nuewvoMensaje").innerHTML += `
     <div class="chat-panel">
     <div class="row no-gutters">
-      <div class="col-md-3">
+      <div class="col-md-3 offset-md-9">
         <div class="chat-bubble chat-bubble--right">
           ${data.mensaje}
         </div>
